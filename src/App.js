473efe6cd9e6fcadacdc522a3661102332fcd8ec
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-08 23:49:16 +0900

    Pseudo Selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 const rotationAnimation = keyframes`
@@ -18,6 +22,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -26,10 +34,9 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
+  ${Emoji} {
     &:hover {
-      font-size: 50px;
+      font-size: 98px;
     }
   }
 `;
@@ -38,7 +45,7 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>😎</span>
+        <Emoji>😎</Emoji>
       </Box>
     </Wrapper>
   );
